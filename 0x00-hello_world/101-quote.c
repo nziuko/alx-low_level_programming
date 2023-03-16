c8b08609bfeae363925597ee1380ece798fdcc09
nziuko <catherine.nziuko@gmail.com>
2023-03-16 14:50:58 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
+ *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n," stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n," 59);
 return (1);
 }
