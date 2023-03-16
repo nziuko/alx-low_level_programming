6bab1428934f4466b4c22c58ecb2feed9c73f319
nziuko <catherine.nziuko@gmail.com>
2023-03-16 13:13:59 -0700

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that prints a line using the printf function
  *
- * Return: 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
