45130c348c1e56750bd1658068367901584875a2
nziuko <catherine.nziuko@gmail.com>
2023-03-16 15:47:12 -0700

    Q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - main function
- *
- * Return: 0 on success
+ * Return: 1 on success
  */
+
 int main(void)
 {
-	write("echo 'and that piece of art is useful\" \ - Dora Korpar, 2015-10-19'");
-return (1);
-}
+	write(2, "and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+
