16cb03ee2a1f08fe13bc60dd07555b644700300c
nziuko <catherine.nziuko@gmail.com>
2023-03-17 05:48:52 -0700

    Size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function
+ * main -Entry point
  * Return 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
