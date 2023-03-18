955555e5fa2aababb84b61cfd497f03902d92ac0
nziuko <catherine.nziuko@gmail.com>
2023-03-17 19:16:04 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,12 +16,12 @@ int main(void)
 	int f_d2;
 	int l_d2;
 
-	while (c <= 98)
+	while (c <= 10)
 	{
 		f_d = (c / 10 + '0');
 		l_d = (c % 10 + '0');
 		c2 = 0;
-		while (c2 <= 99)
+		while (c2 <= 00)
 		{
 			f_d2 = (c2 / 10 + '0');
 			l_d2 = (c2 % 10 + '0');
