4f819f5dbe18accb193759b72c1fe08c6bd9130e
nziuko <catherine.nziuko@gmail.com>
2023-03-18 22:59:29 -0700

    Q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 	{
 		const char a = "and that piece of art is useful\"";
 		"- Dora Korpar, 015-10-19\n";
-		write(STDOUT_FILENO, a, sizeof(a) - 1);
+		fwrite(STDOUT_FILENO, a, sizeof(a) - 1);
 		return (1);
 	}
