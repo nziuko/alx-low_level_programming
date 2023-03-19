64cb481474333a4bba3b7d42fa73f7f6d81d3b40
nziuko <catherine.nziuko@gmail.com>
2023-03-19 15:12:21 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,7 @@
  */
 
 int main(void)
-	{
-fputs("and that piece of art is useful\"
-- Dora Korpar, 2015-10-19", stdout);
-putchar(' ')
-return (1);
-	}
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59);
+	return (1);
+}
