9825b2c40bb306ffdd504a35a0546df615d2901a
nziuko <catherine.nziuko@gmail.com>
2023-03-19 15:55:15 -0700

    Q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,5 @@ int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korrpar, 2015-10-19\n",
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1):
+return (1);
 }
