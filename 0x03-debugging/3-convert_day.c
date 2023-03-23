0c3dea2b0e2a4e720cecc7530369760437445a11
nziuko <catherine.nziuko@gmail.com>
2023-03-23 01:57:53 -0700

    Leap

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -13,7 +13,7 @@ int convert_day(int month, int day)
 switch (month)
 {
 case 2:
-day = 31 + day
+day = 31 + day;
 break;
 case 3:
 day = 59 + day;
@@ -24,13 +24,13 @@ break;
 case 5:
 day = 120 + day;
 break;
-case 6;
+case 6:
 day = 151 + day;
 break;
 case 7:
 day = 181 + day;
 break;
-case 8;
+case 8:
 day = 212 + day;
 break;
 case 9:
@@ -39,13 +39,13 @@ break;
 case 10:
 day = 273 + day;
 break;
-case 11;
+case 11:
 day = 304 + day;
 break;
 case 12:
 day = 334 + day;
 break;
-default
+default:
 break;
 }
 return (day);
