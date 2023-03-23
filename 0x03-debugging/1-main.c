2743541f47a31c84e51df23301051f092f878434
nziuko <catherine.nziuko@gmail.com>
2023-03-23 00:30:25 -0700

    Main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,13 +4,10 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
-
 printf("infinite loop incoming :(\n");
-
 i = 0;
 /*
 *while (i < 10)
@@ -19,6 +16,5 @@ i = 0;
 *}
 */
 printf("infinite loop avoided! \\o/\n");
-
 return (0);
 }
